341e1928c4d3ea8953eaf7015f350497313f020b
Abhishek-Beta <Abhishek-Beta@users.noreply.github.com>
2024-12-03 11:06:35 +0000

    Deploy preview for PR 26 🛫

---
+++ pr-preview/pr-26/assets/js/00ffc1f8.0dc91e39.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbetalectic_website=self.webpackChunkbetalectic_website||[]).push([["27"],{7242:function(e){e.exports=JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-26/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
