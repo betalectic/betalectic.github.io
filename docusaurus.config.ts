da98606ab02755a7ca5a4a68962f84ae1932aad6
Rajiv Seelam <rajiv@betalectic.com>
2025-01-18 19:42:34 +0530

    Update site URL and add Google site verification file

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://betalectic.github.io",
+  url: "https://betalectic.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: BASE_URL,
