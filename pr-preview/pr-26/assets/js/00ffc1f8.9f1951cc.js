1455f38ec583078b9c60ff0cea0d1f599bdb77cb
ShreyDhyani <ShreyDhyani@users.noreply.github.com>
2025-02-18 13:37:39 +0000

    Remove preview for PR 26 🛬

--- pr-preview/pr-26/assets/js/00ffc1f8.9f1951cc.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkbetalectic_website=self.webpackChunkbetalectic_website||[]).push([["2027"],{7242:function(e){e.exports=JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-26/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":5,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
--- pr-preview/pr-26/assets/js/1824.f63b333e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkbetalectic_website=self.webpackChunkbetalectic_website||[]).push([["1824"],{1764:function(e,t,c){c.d(t,{createPieServices:function(){return i.l}});var i=c(5243);c(5318)}}]);
