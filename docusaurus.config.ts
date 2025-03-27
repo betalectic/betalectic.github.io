df2eb58269af22f95fad1b99582f05a6b4e13f61
Rajiv Seelam <rajiv@betalectic.com>
2025-03-27 20:25:01 +0530

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -84,7 +84,7 @@ const config: Config = {
 
   scripts: [
     {
-      src: "//cdn.jsdelivr.net/npm/meta-scan@0.10.2/dist/auto.global.js",
+      src: "//cdn.jsdelivr.net/npm/meta-scan@0.11.0/dist/auto.global.js",
       "data-auto-enable": "false",
       crossorigin: "anonymous",
     },
